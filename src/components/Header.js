94fd0e7f4e0910b784428570faa371c8d41ef5a9
Ishika Gupta <apple@Ishikas-Macbook-Air.local>
2024-09-29 09:39:10 +0530

    SideBar, Header and Navbar UI

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header(){
+return(
+<>
+<div className=" flex justify-between mt-[70px] items-center">
+<div className="flex gap-2 items-center">
+<img src="./rightArr.svg" className="w-8"></img>
+<div className="flex flex-col">
+<p className="font-semibold text-3xl">Rules Creation</p>
+<hr className="border-black mt-1 w-96"></hr>
+</div>
+</div>
+<div>
+<button className="bg-green-500 text-white rounded-md p-3 font-medium">Publish Feed</button>
+</div>
+</div>
+</>
+)
+}
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function NavBar(){
+return(
+<>
+<div className="bg-black w-full h-[70px] fixed">hii</div>
+</>
+)
+}
+
+export default NavBar;
